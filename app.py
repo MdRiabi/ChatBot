96a1043c50570e0e04af013cb5a7c2758525c925
MdRiabi <riabymohamed@gmail.com>
2025-06-05 17:42:35 +0100

    add PDF and TXT file analysis functionality with summarization and information extraction

--- app.py
+++ app.py
@@ -35,10 +35,11 @@ if st.button("send") and user_input:
 
 
 '''
-
+import os
 import streamlit as st 
 from groq_api import ask_groq
 from db import init_db, get_or_create_user, get_history, save_message
+from PyPDF2 import PdfReader
 init_db()
 if "history" not in st.session_state:
     st.session_state.history = []
@@ -86,3 +87,44 @@ if "user_id" in st.session_state:
             for msg in st.session_state.history:
                 role = "🧑 You" if msg["role"] == "user" else "🤖 IA"
                 st.markdown(f"**{role}** : {msg['content']}")
+
+
+
+
+# PDF TXT FILE ANALYSE
+
+st.subheader("📁 File Analyser")
+upload_file = st.file_uploader("Upload A PDF & TXT & .chat File", type=["pdf", "txt", "chat"])
+
+if upload_file:
+    file_ext = os.path.splitext(upload_file)[1].lower()
+
+    # reading the content of file
+    text = ""
+    if file_ext == ".pdf":
+        pdf = PdfReader(upload_file)
+        for page in  pdf.pages:
+            text += page.extract_text()
+    elif file_ext in [".txt", ".chat"]:
+        text = upload_file.read().decode("utf-8")
+
+
+
+
+# display a plain text
+st.text_area("📝 extracted content  :", text[:2000], height=200)
+
+#resume
+if st.button("📌 Summarize the File"):
+    prompt_summary = f"Here is some text extracted from a file:\n\n{text[:5000]}\n\nMake a clear and concise summary."
+    summary = ask_groq(prompt_summary)
+    st.success("📋 Summary  :")
+    st.write(summary)
+
+
+# Information Extraction
+if st.button("🔍  Extract important information :"):
+    prompt_infos = f"Here is a text:\n\n{text[:5000]}\n\nHighlight the essential information or key points in the form of bullet points."
+    points = ask_groq(prompt_infos)
+    st.success("📌  Key points   ::")
+    st.markdown(points)
